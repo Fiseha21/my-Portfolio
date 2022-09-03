8565cea7a70be565438c0a0bf897ba5eb87d1459
Fiseha <fisehagebremedhn1@gmail.com>
2022-09-03 12:09:45 +0300

    new components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
+import { SkillsComponent } from './skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "homepage", redirectTo: "/", pathMatch: "full" },
+  { path: "", component: HomeComponent },
+  { path: "about", component: AboutComponent },
+  { path: "contact", component: ContactComponent },
+  { path: "services", component: ServicesComponent },
+  { path: "skills", component: SkillsComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
